b818e1f233b6ce9bfa88249aeebbb98c4424f497
Acxell Gonzalez <hilabush98@hotmail.com>
2024-10-24 12:31:09 -0600

    Ignnore files conteiner an nginx conf

--- provider-components/src/components/UserSettings/UserSettingsComponent.jsx
+++ provider-components/src/components/UserSettings/UserSettingsComponent.jsx
@@ -1,51 +1,51 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 import { Avatar, Popover } from 'antd';
 import CardComponent from '../Card/CardComponent';
 import { getIcon } from '../../Utils/Icons/getIconFunction';
 
-export const UserSettingsComponent = ({avatarProps, cardProps}) => {
-    const [open, setOpen] = useState(false);
+export const UserSettingsComponent = ({ avatarProps, cardProps }) => {
+  const [open, setOpen] = useState(false);
 
-    const handleOnRefresh = () => {
-        console.log('Generar un nuevo token')        
-    };
-    const logout = () => {
-        console.log('Desloguearte')
-    };
-    const handleOpenChange = (newOpen) => {
-        setOpen(newOpen);
-    };
+  const handleOnRefresh = () => {
+    console.log('Generar un nuevo token');
+  };
+  const logout = () => {
+    console.log('Desloguearte');
+  };
+  const handleOpenChange = (newOpen) => {
+    setOpen(newOpen);
+  };
   return (
-<Popover
-            padding={0}
-            placement="bottom"
-            trigger="click"
-            style={{border:'solid'}}
-            open={open}
-            overlayStyle={{ padding: 0 }}
-            onOpenChange={handleOpenChange}
-            content={
-                <>
-                    <CardComponent
-                        typeCard={'userSettings'}
-                        userName={'Acxell Gonzalez'}
-                        depto={'Corporativo MTY'}
-                        handleOnRefresh={handleOnRefresh}
-                        handleLogout={logout}
-                        userInfo={''}
-                        LoadingRefresh={false}
-                        expiration_token={0}
-                    />
-                </>
-            }
-        >
-            <Avatar
-                size={avatarProps?.size}
-                icon={avatarProps?.icon || getIcon('UserOutlined')}
-                style={avatarProps.style}
-            />
-        </Popover>
-  )
-}
+    <Popover
+      padding={0}
+      placement="bottom"
+      trigger="click"
+      style={{ border: 'solid' }}
+      open={open}
+      overlayStyle={{ padding: 0 }}
+      onOpenChange={handleOpenChange}
+      content={
+        <>
+          <CardComponent
+            typeCard={'userSettings'}
+            userName={'Acxell Gonzalez'}
+            depto={'Corporativo MTY'}
+            handleOnRefresh={handleOnRefresh}
+            handleLogout={logout}
+            userInfo={''}
+            LoadingRefresh={false}
+            expiration_token={0}
+          />
+        </>
+      }
+    >
+      <Avatar
+        size={avatarProps?.size}
+        icon={avatarProps?.icon || getIcon('UserOutlined')}
+        style={avatarProps?.style}
+      />
+    </Popover>
+  );
+};
 
-export default UserSettingsComponent
+export default UserSettingsComponent;
